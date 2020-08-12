c2ecdaa221b5e4c0178501ccf61aaf6697a5aeea
Daniel Ramos de Carvalho <danielramosbh74@gmail.com>
2020-08-12 15:00:48 -0300

    updating

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_c__helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
